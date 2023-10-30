40de2364a3591ff00ba479e925cac543a0cd6aae
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:57:28 +0100

    Rama1 modificadaç

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         float x = 3f;
+        System.out.println("Modificando la rama:");
+        x*=3*x;
+        System.out.println(x);
     }
 }
